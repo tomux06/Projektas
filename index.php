550c5a8b3aef446ef4ee9e16043a5df34553fcf2
tomux06 <tomux06@inbox.lt>
2023-03-14 22:59:04 +0200

    komentaras

---
+++ index.php
@@ -0,0 +1,79 @@
+<?php
+    //echo 'Zdarova';
+   // print_r([1,2,3]);
+   //var_dump('Hello');
+   //var_dump(true);
+   //var_export('Hello');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<ul>
+   <li> <?php
+    $salis = 'Lietuva';
+   
+    echo $salis;
+    ?> 
+    </li>
+
+    <li>
+    <?php
+     $miestas ='Vilnius';
+     
+     echo $miestas;
+    ?>
+    </li>
+    <li>
+    <?php
+         $populiacija ="546212";
+         echo $populiacija;
+    ?>
+    </li>
+
+
+    <?php
+    echo $miestas . ' Lietuvos sostine.';
+    
+    ?>
+    <br>
+    <?php
+      echo "$miestas ir $salis sparciai auga.";
+    ?>
+   <br>
+
+    <?php
+   $x=10; $y=7; $z=0;
+  echo  $z=$x+$y;
+    ?><br>
+        <?php
+   $x=10; $y=7; $z=0;
+ 
+  echo  $z=$x-$y;
+    ?><br>
+        <?php
+   $x=10; $y=7; $z=0;
+ 
+  echo  $z=$x*$y;
+    ?><br>
+        <?php
+   $x=10; $y=7; $z=0;
+ 
+  echo  $z=$x/$y;
+    ?><br>
+        <?php
+   $x=10; $y=7; $z=0;
+ 
+  echo  $z=$x%$y;
+    ?>
+</ul>
+</body>
+</html>gi
